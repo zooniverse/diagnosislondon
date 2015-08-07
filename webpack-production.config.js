4c612bee44a00d9954940c2ba634daa6fd5cbb3a
Jim O'Donnell <jim@zooniverse.org>
2015-08-07 16:59:05 +0100

    Replace jsx-loader with babel-loader

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -19,7 +19,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.cjsx$/, loaders: ['coffee-loader', 'cjsx-loader'] },
-      { test: /\.jsx$/, loader: 'jsx-loader'},
+      { test: /\.jsx$/, loader: 'babel-loader'},
       { test: /\.coffee$/, loader: 'coffee-loader' },
       { test: /\.json$/, loader: 'json-loader' },
     ],
